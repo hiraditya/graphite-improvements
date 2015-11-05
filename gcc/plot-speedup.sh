bab3b4ed491696c0ce3723644e6a032018021e47
Sebastian Pop <sebpop@gmail.com>
2015-11-05 13:50:42 -0600

    add new data

--- gcc/plot-speedup.sh
+++ gcc/plot-speedup.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
+sortit() {
+    sort -k2 -n $1 > sorted-$1
+    cat sorted-$1 | cut -d ',' -f 2 > $1
+}
+
 collect () {
     > speedup.txt
+    > old-speedup.txt
+    > new-speedup.txt
     for i in $(grep -vre ': $' new/report.txt | cut -d ':' -f 1); do
         old=$(grep "^$i" old/report.txt | cut -d ':' -f 2)
         new=$(grep "^$i" new/report.txt | cut -d ':' -f 2)
-        speedup=$(echo "scale=2; ($old / $new)" | bc)
-        echo $(echo $i | sed -e 's/callgrind.out.//g'), $speedup &>> speedup.txt
+        main=$(grep "^$i" old/main.txt | cut -d ':' -f 2)
+        pold=$(echo "scale=4; ($old / $main) * 100" | bc)
+        pnew=$(echo "scale=4; ($new / $main) * 100" | bc)
+        diff=$(echo "scale=4; $pold - $pnew" | bc)
+        #speedup=$(echo "scale=2; ($old / $new)" | bc)
+        #echo $(echo $i | sed -e 's/callgrind.out.//g'), $speedup &>> speedup.txt
+        echo $(echo $i | sed -e 's/callgrind.out.//g'), $pold &>> old-speedup.txt
+        echo $(echo $i | sed -e 's/callgrind.out.//g'), $pnew &>> new-speedup.txt
     done
-    sort -k2 -n speedup.txt > speedup-sorted.txt
-    cat speedup-sorted.txt | cut -d ',' -f 2 > speedup.txt
+    #sortit speedup.txt
+    sortit old-speedup.txt
+    sortit new-speedup.txt
 }
 
-collect
+#collect
 gnuplot plot.gp
 
--- gcc/plot.gp
+++ gcc/plot.gp
@@ -1,8 +1,8 @@
 set terminal latex
-unset key
-set logscale y 10
+set key top left
+#set logscale y 2
 set xlabel "Files of GCC 6.0"
-set ylabel "Speedup"
+set ylabel "\\rotatebox{90}{\\% of overall compilation time}"
 set output "speedup.tex"
 set datafile separator ","
-plot "speedup.txt" with impulses
+plot "old-speedup.txt" with points pointtype 1 title "old SCoP detection", "new-speedup.txt" with points pointtype 7 title "new SCoP detection"
